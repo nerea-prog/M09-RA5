5e9824168789d786fcce69107aae7ca2d90a553d
nerea-prog <narvaeznerea18@gmail.com>
2025-09-29 18:58:55 +0200

    02-RotX practica acabada

--- 02-RotX/src/RotX.java
+++ 02-RotX/src/RotX.java
@@ -11,31 +11,25 @@ Crea també un main per fer algunes proves.
  */
 public class RotX {
 
-    // Array de las letras del abecedario en minusculas
     static char[] arrayAbecedari = "aáàäbcçdeéèëfghiíìïjklmnñoóòöpqrstuúùüvwxyz".toCharArray();
-    // Array de las letras del abecedario en mayusculas
     static char[] arrayAbecedariM = "aáàäbcçdeéèëfghiíìïjklmnñoóòöpqrstuúùüvwxyz".toUpperCase().toCharArray();
 
     public static void main(String[] args) {
-        // Array de strings con las pruebas
         String[] proves = { "ABC", "XYZ", "Hola, Mr. calçot",
                 "Perdó, per tu què és?" };
 
         System.out.println("------- CIFRADO ---------");
-        for (int i = 0; i < proves.length; i++) { // recorremos la array de pruebas de string
-            String cadena = proves[i]; // cogemos la string que queremos tratar
-            String cadenaCifrada = xifraRotX(cadena, i * 2); // ciframos la cadena
-            System.out.println("(" + i * 2 + ")-" + cadena + " --> " + cadenaCifrada); // printeamos el resultado por
-                                                                                       // pantalla
+        for (int i = 0; i < proves.length; i++) {
+            String cadena = proves[i];
+            String cadenaCifrada = xifraRotX(cadena, i * 2);
+            System.out.println("(" + i * 2 + ")-" + cadena + " --> " + cadenaCifrada);
         }
         System.out.println("------ DESCRIFRADO --------");
-        for (int i = 0; i < proves.length; i++) { // recorremos la array de pruebas de string
-            String cadena = proves[i]; // cogemos la string que queremos tratar
-            String cadenaCifrada = xifraRotX(cadena, i * 2); // ciframos la cadena
-            String cadenaDescifrada = desxifraRotX(cadenaCifrada, i * 2); // descriframos la cadena que tenemos cifrada
-            System.out.println("(" + i * 2 + ")-" + cadenaCifrada + " --> " + cadenaDescifrada); // printeamos el
-                                                                                                 // resultado por
-                                                                                                 // pantalla
+        for (int i = 0; i < proves.length; i++) {
+            String cadena = proves[i];
+            String cadenaCifrada = xifraRotX(cadena, i * 2);
+            String cadenaDescifrada = desxifraRotX(cadenaCifrada, i * 2);
+            System.out.println("(" + i * 2 + ")-" + cadenaCifrada + " --> " + cadenaDescifrada);
             if (i == proves.length - 1) {
                 forcaBrutaRotX(cadenaCifrada);
             }
@@ -47,34 +41,26 @@ public class RotX {
      * y nos devolvera el texto cifrado
      */
     public static String xifraRotX(String cadena, int desplaçament) {
-        String textoCifrado = ""; // String para guardar el cifrado
-        for (int i = 0; i < cadena.length(); i++) { // Recorremos la cadena
-            char caracter = cadena.charAt(i); // cogemos el caracter que queremos tratar
-            // llamamos al metodo que nos busca la posicion del caracter en el abecedario de
-            // minusculas
+        String textoCifrado = "";
+        for (int i = 0; i < cadena.length(); i++) {
+            char caracter = cadena.charAt(i);
             int posicio = buscaLletra(arrayAbecedari, caracter);
             if (posicio != -1) { // si la posicion no es -1, es decir que se encuentra en el abecedario
                 // cogemos la nueva posicion sumandole 13 y hacemos el modulo para no pasarnos
                 // de posicion
                 int posicionNueva = (posicio + desplaçament) % arrayAbecedari.length;
-                // busca esa posicion en el abecedario y añade el caracter de esa posicion en la
-                // string
                 textoCifrado += arrayAbecedari[posicionNueva];
-                // si no se ha encontrado en el abecedario de minusculas haremos el mismo
-                // proceso en el abecedario de mayusculas
             } else {
                 posicio = buscaLletra(arrayAbecedariM, caracter);
                 if (posicio != -1) {
                     int posicionNueva = (posicio + desplaçament) % arrayAbecedariM.length;
                     textoCifrado += arrayAbecedariM[posicionNueva];
-                    // Si no se ha encontrado en ninguno de los dos abecedarios, añadiremos el
-                    // caracter tal cual en caso de espacios, comas...
                 } else {
                     textoCifrado += caracter;
                 }
             }
         }
-        return textoCifrado; // devuelve la cadena cifrada
+        return textoCifrado;
     }
 
     /*
@@ -82,11 +68,9 @@ public class RotX {
      * y nos devolvera el texto descrifrado
      */
     public static String desxifraRotX(String cadena, int desplaçament) {
-        String textoDescrifrado = ""; // String para guardar el cifrado
-        for (int i = 0; i < cadena.length(); i++) { // Recorremos la cadena
-            char caracter = cadena.charAt(i); // cogemos el caracter que queremos tratar
-            // llamamos al metodo que nos busca la posicion del caracter en el abecedario de
-            // minusculas
+        String textoDescrifrado = "";
+        for (int i = 0; i < cadena.length(); i++) {
+            char caracter = cadena.charAt(i);
             int posicio = buscaLletra(arrayAbecedari, caracter);
             if (posicio != -1) { // si la posicion no es -1, es decir que se encuentra en el abecedario
                 // Cogemos la nueva posicion restandole 13 para descrifrar y sumandole la
@@ -94,18 +78,12 @@ public class RotX {
                 // Hacemos el modulo por si la suma de la longitud se pasa de la longitud de la
                 // array del abecedario
                 int posicionNueva = (posicio - desplaçament + arrayAbecedari.length) % arrayAbecedari.length;
-                // busca esa posicion en el abecedario y añade el caracter de esa posicion en la
-                // string
                 textoDescrifrado += arrayAbecedari[posicionNueva];
-                // si no se ha encontrado en el abecedario de minusculas haremos el mismo
-                // proceso en el abecedario de mayusculas
             } else {
                 posicio = buscaLletra(arrayAbecedariM, caracter);
                 if (posicio != -1) {
                     int posicionNueva = (posicio - desplaçament + arrayAbecedari.length) % arrayAbecedariM.length;
                     textoDescrifrado += arrayAbecedariM[posicionNueva];
-                    // Si no se ha encontrado en ninguno de los dos abecedarios, añadiremos el
-                    // caracter tal cual en caso de espacios, comas...
                 } else {
                     textoDescrifrado += caracter;
                 }
@@ -113,12 +91,16 @@ public class RotX {
             }
 
         }
-        return textoDescrifrado; // devuelve la cadena descrifrada
+        return textoDescrifrado;
     }
+    /*
+     * Esta funcion le pasamos la string cifrada por parametro
+     * Imprimira esa string
+     * Recorrera la longitud del abecedario e imprimira cada descifrado que se vaya haciendo
+     */
 
     public static void forcaBrutaRotX(String cadenaCifrada) {
         System.out.println("\nMissatge xifrat: " + cadenaCifrada + "\n");
-        String textoDescifrado = "";
         for (int i = 0; i < arrayAbecedari.length; i++) {
             System.out.println("(" + i + ") " + desxifraRotX(cadenaCifrada, i));
         }
@@ -130,12 +112,12 @@ public class RotX {
      */
     
     public static int buscaLletra(char[] abecedari, char lletra) {
-        for (int i = 0; i < abecedari.length; i++) { // recorremos el abecedario
-            char caracter = abecedari[i]; // Cogemos el caracter del abecedario
-            if (lletra == caracter) { // Si la letra de nuestra cadena coincide con la del abecedario
-                return i; // devolvemos la posicion en la que esta
+        for (int i = 0; i < abecedari.length; i++) { 
+            char caracter = abecedari[i];
+            if (lletra == caracter) {
+                return i; 
             }
         }
-        return -1; // si no se encuentra devolveremos -1
+        return -1;
     }
 }
